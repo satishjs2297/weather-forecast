1014637d2d7483fac7b56931c15f12d4ed6da997
Satish Y <satishjs2297@gmail.com>
2019-10-02 17:39:55 +0530

    add docs

--- src/main/java/com/weather/informer/constants/WeatherConstants.java
+++ src/main/java/com/weather/informer/constants/WeatherConstants.java
@@ -10,6 +10,6 @@ public interface WeatherConstants {
 	String GET_WEATHER_INFO = "/getWeatherInfoByCities/{cities}";
 
 	// Open weather url
-	String OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?APPID=b1ae276535f95b4c18183f92b7e3e6a6&q=";
+	String OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?APPID=1de37415bca57f4bc3e119bb810ade0d&q=";
 
 }
